1e1db6774226548e985fd14074423fdcbae3327b
Hynes <81213065+Hynes1154@users.noreply.github.com>
2022-03-08 00:04:21 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
+var ovar = document.getElementById('game-over')
+
 function jump(){
     if(character.classList == "animate"){return}
     character.classList.add("animate");
@@ -8,14 +10,21 @@ function jump(){
         character.classList.remove("animate");
     },300);
 }
+
+document.addEventListener('keyup', event => {
+    if (event.code === 'Space') {
+        jump()
+    }
+    })
+
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
         block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
+        ovar.innerText = 'Game Over! Score: ' + Math.floor(counter/100)
         counter=0;
-        block.style.animation = "block 1s infinite linear";
+        block.style.animation = "block 2s infinite linear";
     }else{
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
